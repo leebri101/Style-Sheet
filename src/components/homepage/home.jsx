b7fe4775facfba5339fea39830e33f0d665801b7
Brian Lee <brian-lee@live.co.uk>
2024-10-15 19:36:30 +0100

    Basic structure of home page added

---
+++ src/components/homepage/home.jsx
@@ -0,0 +1,12 @@
+
+
+const Home = () => {
+    return (
+        <div>
+            <h1>Welcome to the Homepage</h1>
+            <p>This is the homepage of our app.</p>
+        </div>
+    );
+};
+
+export default Home;
