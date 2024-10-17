a8450fde74864636fa6e3cf1f8c54c5b074599d5
Brian Lee <brian-lee@live.co.uk>
2024-10-17 13:55:48 +0100

    Removal of unwanted code

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,23 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import Navbar from './components/layout/navbar'
+import Home from './components/homepage/home';
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [count, setCount] = useState(0);
   return (
-    <>
-    <Navbar />
-      <div>
-      <h1>Style-Sheet</h1>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+    <div>
+      <Home />
+      <Navbar />
+      <main className="card">
+        <br />
+        <button onClick={() => setCount(count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      </main>
+    </div>
+  );
 }
 
-export default App
-
+export default App;
 
