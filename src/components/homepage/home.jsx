1b66df90efa51437a051c3c7b6e5ecac692ca275
Brian Lee <brian-lee@live.co.uk>
2024-10-18 11:36:59 +0100

    Removal of home menu tab

--- src/components/homepage/home.jsx
+++ src/components/homepage/home.jsx
@@ -1,23 +1,10 @@
-import {Menu, MenuButton, MenuItem, MenuGroup, MenuList, Button } from '@chakra-ui/react'
 
 const Home = () => {
     return (
         <div>
             <h1>Style-Sheet</h1>
             <p>Find all the unique styles that you need right here... </p>
-            <Menu>
-                <MenuButton as={Button}>
-                    Account
-                </MenuButton>
-                <MenuList>
-                    <MenuGroup>
-                        <MenuItem>Login</MenuItem>
-                        <MenuItem>Register</MenuItem>
-                    </MenuGroup>
-                </MenuList>
-            </Menu>
         </div>
     );
 };
-
 export default Home;
