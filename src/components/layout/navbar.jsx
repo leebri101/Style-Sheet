4bb69492b5844189a9ee60a84b0daa17443ee841
Brian Lee <brian-lee@live.co.uk>
2024-10-18 16:51:08 +0100

    Change of custom color array

--- src/components/layout/navbar.jsx
+++ src/components/layout/navbar.jsx
@@ -1,19 +1,17 @@
 import { useState } from 'react'
 import {Tabs, TabList, Tab, useColorMode,  Menu, MenuButton, MenuItem, MenuGroup, MenuList, Button} from '@chakra-ui/react'
 
-
 function Links() {
-  const { colorMode } = useColorMode();
-  const lightColors = ['#ffe5e5', '#e6fffa', '#ebf8ff'];
-  const darkColors = ['#7f1d1d', '#234e52', '#2a4365'];
-  const colors = colorMode === 'light' ? lightColors : darkColors;
+  const  colors = useColorMode(
+    ['#ffe5e5', '#e6fffa', '#ebf8ff'],
+    ['#7f1d1d', '#234e52', '#2a4365']
+  );
   const [tabIndex, setTabIndex] = useState(0);
   const bg = colors[tabIndex];
-
   return (
     <div>
       <Tabs onChange={(index) => setTabIndex(index)} bg={bg}>
-        <TabList style={{ position: 'relative' }}>
+        <TabList>
           <Tab mr={5}>Products</Tab>
           <Tab mr={5}>My Styles</Tab>
           <Tab mr={5}>Basket</Tab>
