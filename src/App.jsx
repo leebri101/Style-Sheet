57c150f7dfe475b7f54ad640ea7abf35167b0312
Brian Lee <brian-lee@live.co.uk>
2024-11-04 13:56:34 +0000

    Change of main home page destionation to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import {Tabs, TabList, Tab, useColorMode,  Menu, MenuButton, MenuItem, MenuGroup, MenuList, Button} from '@chakra-ui/react'
 import './App.css'
 import Home from "./components/homepage/Home";
@@ -7,7 +7,6 @@ import Register from "./components/user/Register";
 
 
 function App() {
-  const [count, setCount] = useState(0);
   const colors = useColorMode(
     ['#ffe5e5', '#e6fffa', '#ebf8ff'],
     ['#7f1d1d', '#234e52', '#2a4365']
@@ -17,31 +16,25 @@ function App() {
 
   return (
     <div>
-      <Router>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/register" element={<Register />} />
         </Routes>
-      </Router>
-      <main className="card">
-        <br />
-        <button onClick={() => setCount(count + 1)}>
-          count is {count}
-        </button>
-      </main>
       <div>
         <Tabs onChange={(index) => setTabIndex(index)} bg={bg}>
           <TabList>
-            <Tab mr={5}>Products</Tab>
-            <Tab mr={5}>My Styles</Tab>
-            <Tab mr={5}>Basket</Tab>
+            <Tab mr={10}>Products</Tab>
+            <Tab mr={10}>My Styles</Tab>
+            <Tab mr={10}>Basket</Tab>
             <Menu>
               <MenuButton as={Button}>
                 Account
               </MenuButton>
               <MenuList>
                 <MenuGroup>
+                  <MenuItem>Profile</MenuItem>
                   <MenuItem>Login</MenuItem>
+                  <MenuItem>Logout</MenuItem>
                   <MenuItem onClick={() => window.location.href = '/register'}>Register</MenuItem>
                 </MenuGroup>
               </MenuList>
