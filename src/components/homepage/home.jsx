3ff69f30bfb6549af67a63924a3a5356f381ddb8
Brian Lee <brian-lee@live.co.uk>
2024-11-12 11:38:28 +0000

    Changes of parent code

--- src/components/homepage/home.jsx
+++ src/components/homepage/home.jsx
@@ -1,9 +1,9 @@
 const Home = () => {
     return (
-        <div>
+        <header>
             <h1>Style-Sheet</h1>
             <p>Find all the unique styles that you need. </p>
-        </div>
+        </header>
     );
 };
 export default Home;
