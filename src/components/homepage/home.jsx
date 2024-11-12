bf74a47b60cd85b5daf1c4e94fa9c19abe383e05
Brian Lee <brian-lee@live.co.uk>
2024-11-12 13:28:02 +0000

    Temporary disable of home comp to edit

--- src/components/homepage/home.jsx
+++ src/components/homepage/home.jsx
@@ -1,4 +1,4 @@
-const Home = () => {
+{/*const Home = () => {
     return (
         <header>
             <h1>Style-Sheet</h1>
@@ -6,4 +6,4 @@ const Home = () => {
         </header>
     );
 };
-export default Home;
+export default Home; */}    //temporarily disabled Home component
