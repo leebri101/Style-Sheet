90ee6ed97213c85b95b275b858dc9903e39c888c
Brian Lee <brian-lee@live.co.uk>
2024-12-27 15:41:42 +0000

    Rename of comp name and creation of css file

--- src/components/layout/footer.jsx
+++ src/components/layout/footer.jsx
@@ -1,5 +1,6 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faFacebook, faXTwitter, faInstagram, } from '@fortawesome/free-brands-svg-icons';
+import './Footer.css';
 
 const Footer = () => {
     return (
