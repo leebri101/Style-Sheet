205b8218ae01b4e1cc96e5d51948daf47133eba6
Brian Lee <brian-lee@live.co.uk>
2024-12-30 14:57:02 +0000

    Change of styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import { useState } from 'react';
 import { Routes, Route, useNavigate, } from 'react-router-dom';
-import { Tabs, TabList, Tab, useColorMode, Menu, MenuButton, MenuItem, MenuGroup, MenuList } from '@chakra-ui/react';
+import { Tabs, TabList, Tab, useColorMode, Menu, MenuButton, MenuItem, MenuGroup, MenuList, Button } from '@chakra-ui/react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faShoppingBasket, faUser , faHeartCirclePlus, faMagnifyingGlassDollar, faShirt } from '@fortawesome/free-solid-svg-icons';
-import { Button } from '@chakra-ui/react';
+import { faShoppingBasket, faUser , faHeartCirclePlus, faMagnifyingGlass, faShirt } from '@fortawesome/free-solid-svg-icons';
 import Register from "./components/user/Register";
 import Footer from './components/layout/Footer'; 
-import Products from './components/products/Products';
 import Logo from './components/ui/Logo';
 import './App.css';
 
@@ -31,11 +29,13 @@ function App() {
       case 2:
         navigate('/basket');
         break;
+      case 3:
+        navigate('/products');
+        break;
       default:
         break;
     }
   };
-
   return (
     <div>
       <div className="tabs-container">
@@ -45,20 +45,20 @@ function App() {
               <Logo />
             </div>
             <Tab mr={5} onClick={() => navigate('/search')}>
-              <FontAwesomeIcon icon={faMagnifyingGlassDollar} size="xl" />
+            <FontAwesomeIcon icon={faMagnifyingGlass} size="2xl" />
             </Tab>
             <Tab mr={5} onClick={() => navigate('/wishlist')}>
-              <FontAwesomeIcon icon={faHeartCirclePlus} size="xl" />
+              <FontAwesomeIcon icon={faHeartCirclePlus} size="2xl" />
             </Tab>
             <Tab mr={5} onClick={() => navigate('/basket')}>
-              <FontAwesomeIcon icon={faShoppingBasket} size="xl" />
+              <FontAwesomeIcon icon={faShoppingBasket} size="2xl" />
             </Tab>
             <Tab mr={5} onClick={() => navigate('/products')}>
-            <FontAwesomeIcon icon={faShirt} size="xl"/>
+            <FontAwesomeIcon icon={faShirt} size="2xl"/>
             </Tab>
             <Menu>
               <MenuButton as={Button} variant="solid" size="md">
-                <FontAwesomeIcon icon={faUser} size="xl" />
+                <FontAwesomeIcon icon={faUser} size="2xl" />
               </MenuButton>
               <MenuList>
                 <MenuGroup>
@@ -75,7 +75,6 @@ function App() {
       <div className="routes-container">
         <Routes>
           <Route path="/register" element={<Register />} />
-          <Route path="/products" element={<Products />} />
           <Route path="*" element={<div>404 Not Found</ div>} />
         </Routes>
       </div>
