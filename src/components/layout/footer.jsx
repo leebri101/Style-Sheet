5332532cff4c3210ed5c841c4f8826f83752d705
Brian Lee <brian-lee@live.co.uk>
2025-01-09 11:22:18 +0000

    Changes to layout

--- src/components/layout/footer.jsx
+++ src/components/layout/footer.jsx
@@ -1,11 +1,11 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faFacebook, faXTwitter, faInstagram, } from '@fortawesome/free-brands-svg-icons';
+import { faFacebook, faXTwitter, faInstagram} from '@fortawesome/free-brands-svg-icons';
 import './Footer.css';
 
 const Footer = () => {
     return (
-        <footer className="footer">
-            <div className="footer-social" >
+        <footer className="footer bg-gray-100">
+            <div className="footer-social max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8" >
                 <p>&copy; {new Date().getFullYear()} Style-Sheet. All rights reserved.</p>
                 <hr />
                 <br />
@@ -20,9 +20,9 @@ const Footer = () => {
                 </a>
                 <br />
                 <div className="other-container">
-                    <a href="/about-us">About Us</a> |
-                    <a href="/contact">Contact</a> |
-                    <a href="/privacy">Returns</a> 
+                    <a href="/about-us" className="text-base text-gray-500 hover:text-gray-900">About Us</a> |
+                    <a href="/contact" className="text-base text-gray-500 hover:text-gray-900">Contact</a> |
+                    <a href="/returns" className="text-base text-gray-500 hover:text-gray-900">Returns</a> 
                 </div>
             </div>
         </footer>
@@ -30,5 +30,3 @@ const Footer = () => {
 };
 
 export default Footer;
-
-
