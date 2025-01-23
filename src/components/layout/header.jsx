95d0b81bdbeeb9f681f293eaa7dd48c90645342c
Brian Lee <brian-lee@live.co.uk>
2025-01-23 11:22:30 +0000

    Link code added into Header

--- src/components/layout/header.jsx
+++ src/components/layout/header.jsx
@@ -14,8 +14,36 @@ const Header= () =>{
 
     return(
         <header className="header">
-            <nav className=''>
-
+            <nav className="header-nav" aria-label="Global">
+                <div className="flex lg:flex-1">
+                    <Link to="/" className="header-logo">
+                    <span className="sr-only">Style</span>
+                    <div>-Sheet</div>
+                    </Link>
+                </div>
+                <div className="flex lg:hidden">
+                    <button type="button" className="header-mobile-menu-button" onClick={() => setMobileMenuOpen(true)}>
+                        <span className="sr-only">Open Main Menu</span>
+                        <Menu className="h-6 w-6" aria-hidden="true" />
+                    </button>
+                </div>
+                <div className="hidden lg:flex lg:gap-x-12">
+                    {navigation.map((item) => (
+                        <Link
+                        key={item.name}
+                        to={item.href}
+                        className="text-sm font-semibold leading-text-gray-900 hover:text-gray-600"
+                        >
+                            {item.name}
+                        </Link>
+                    ))}
+                    <Link to="/login" className="text-sm font-semibold leading-text-gray-900 hover:text-gray-600">
+                    Login 
+                    </Link>
+                    <Link to="/register" className="text-sm font-semibold leading-text-gray-900 hover:text-gray-600">
+                    Register
+                    </Link>
+                </div>
             </nav>
         </header>
     )
