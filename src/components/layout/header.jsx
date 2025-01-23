537acf4ca33b0c3838dc05e2c148db72751d3032
Brian Lee <brian-lee@live.co.uk>
2025-01-23 10:24:46 +0000

    Intial import and code structure formed

--- src/components/layout/header.jsx
+++ src/components/layout/header.jsx
@@ -1,2 +1,22 @@
+import { Menu, Search, ShoppingCart, Heart, Globe } from 'lucide-react';
+import { Dialog } from '@headlessui/react';
+import { Link } from 'react-router-dom';
 import './Header.css';
 
+const Header= () =>{
+    const[mobileMenuOpen, setMobileMenuOpen] = useState(false)
+
+    const navigation = [
+        { name: 'WOMEN', href: '/women' },
+        { name: 'MENs', href: '/mens' },
+        { name: 'KIDS', href: '/kids' },
+    ]
+
+    return(
+        <header className="header">
+            <nav className=''>
+
+            </nav>
+        </header>
+    )
+}
