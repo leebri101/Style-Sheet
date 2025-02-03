9c3e25cc63e37bec9b39f1f7e4eb6e2354ccf7e2
Brian Lee <brian-lee@live.co.uk>
2025-02-03 21:45:17 +0000

    handling added into login page

--- src/components/user/LoginPage.jsx
+++ src/components/user/LoginPage.jsx
@@ -2,4 +2,38 @@ import { useState } from "react"
 import { Link } from "react-router-dom"
 import './Login.css' 
 
-const LoginPage 
+const LoginPage = () => {
+    const [formData, setFormData] = useState({
+        email: "",
+        password: "",
+    })
+
+    const handleChange = (e) => {
+        const { name, value } = e.target
+        setFormData((prevState) => ({
+          ...prevState,
+          [name]: value,
+        }))
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        console.log("Login in Submitted", formData)
+    }
+
+    return (
+        <div className="login-page">
+            <div className="login-form-container">
+                <div>
+                    <h2 className="login-title">
+                        Login in to your account
+                    </h2>
+                    <form>
+                        
+                    </form>
+                </div>
+            </div>
+        </div>
+    )
+    
+}
