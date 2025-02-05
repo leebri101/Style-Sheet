9abaa266857fba792a350d1f38c4e5d093f7801f
Brian Lee <brian-lee@live.co.uk>
2025-02-05 12:57:48 +0000

    export of login page

--- src/components/user/LoginPage.jsx
+++ src/components/user/LoginPage.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react"
 import { Link } from "react-router-dom"
 import './Login.css' 
+import './ForgotPassword.css'
 
 const LoginPage = () => {
     const [formData, setFormData] = useState({
@@ -28,12 +29,69 @@ const LoginPage = () => {
                     <h2 className="login-title">
                         Login in to your account
                     </h2>
-                    <form>
-                        
+                    <form className="login-form" onSubmit={handleSubmit}>
+                        <input type="hidden" name="remember" defaultValue="true"/>
+                        <div className="login-inputs">
+                            <div>
+                                <label htmlFor="email-address" className="sr-only">
+                                    Email-Address:
+                                </label>
+                                <input
+                                id="email-address"
+                                name="email"
+                                type="email"
+                                autoComplete="email"
+                                required
+                                className="login-input"
+                                placeholder="Email Address"
+                                value={formData.email}
+                                onChange={handleChange}
+                                />
+                            </div>
+                            <div>
+                                <label htmlFor="password" className="sr-only">
+                                    Password:
+                                </label>
+                                <input
+                                id="password"
+                                name="password"
+                                type="password"
+                                autoComplete="current-password"
+                                required
+                                className="login-input"
+                                placeholder="Password"
+                                value={formData.password}
+                                onChange={handleChange}
+                                />
+                            </div>
+                            <div className="login-options">
+                                <div className="remember-me" name="remember-me remember-me-checkbox" type="check-box">
+                                    <label htmlFor="remember-me" className="remember-me label"/>
+                                    Remember Me
+                                </div>
+                                <div className="forgot-password">
+                                    <Link to="/forgot-password" className="forgot-password-link">
+                                    Forgot Your Password? 
+                                    </Link>
+                                </div>
+                            </div>
+                            <div>
+                                <button type="submit" className="login-submit">
+                                    Log-in
+                                </button>
+                            </div>
+                        </div>
                     </form>
+                    <div>
+                        <Link to="/register" className="register-link-text">
+                        Don`t have an account Register with us
+                        </Link>
+                    </div>
                 </div>
             </div>
         </div>
     )
     
 }
+
+export default LoginPage
