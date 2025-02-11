0900da36be3cee3ef47314602284a04a7cf28e2f
Brian Lee <brian-lee@live.co.uk>
2025-02-11 12:32:53 +0000

    Fix import path in LogoutPage component

--- src/components/user/LogoutPage.jsx
+++ src/components/user/LogoutPage.jsx
@@ -1,4 +1,4 @@
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
-import { logoutStart, logoutSuccess } from ""
+import { logoutStart, logoutSuccess } from "./authSlice.js"
