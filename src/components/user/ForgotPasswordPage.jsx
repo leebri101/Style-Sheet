fb72d7bc66cb55776977e7eb943bcc3d49e47797
Brian Lee <brian-lee@live.co.uk>
2025-02-19 13:06:05 +0000

    Forgot password comp created in workspace

---
+++ src/components/user/ForgotPasswordPage.jsx
@@ -0,0 +1,58 @@
+// to also do 
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import './ForgotPasswordPage.css';
+
+const ForgotPasswordPage = () => {
+    const [email, setEmail] = useState("");
+    const [isSubmitted, setIsSubmitted] = useState(false);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        
+        console.log("Password Request sent for:", email)
+        setIsSubmitted(true);
+    }
+    return(
+        <div className="forgot-password-page">
+            <div className="forgot-password-container">
+            <h2 className="forgot-password-title">Forgot Password</h2>
+                {!isSubmitted ? (
+                    <form onSubmit={handleSubmit} className="forgot-password-form">
+                        <p className="forgot-password-instruction">
+                            Please enter your Email address to receive a password reset link.
+                        </p>
+                        <div className="form-group">
+                            <label htmlFor="email" className="form-label">
+                                Email Address
+                            </label>
+                            <input 
+                                type="email" 
+                                id="email" 
+                                name="email" 
+                                className="form-input"
+                                value={email} 
+                                onChange={(e) => setEmail(e.target.value)} 
+                            />
+                        </div>
+                        <button type="submit" className="submit-button">
+                            Submit
+                        </button>
+                    </form>
+                    ) : (
+                        <div className="success-message">
+                            <p>If there is an existing account for {email}, 
+                                you will receive your password reset link shortly.</p>
+                        </div>
+                    )}
+                    <div className="back-to-login">
+                        <Link to="/login" className="back-to-login-link">
+                        Back to Login
+                        </Link>
+                    </div>
+            </div>
+        </div>
+    )
+}
+
+export default ForgotPasswordPage;
