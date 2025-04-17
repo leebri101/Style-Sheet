ca6830727058f5f3d685c1ecf7b79ad99f017a63
Brian Lee <brian-lee@live.co.uk>
2025-04-17 18:59:45 +0100

    axios import and config of code

--- src/store/productSlice.js
+++ src/store/productSlice.js
@@ -1,2 +1,38 @@
-//update 
+//to do tomorrow 25/03/25
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+export const fetchProducts = createAsyncThunk(
+    'products/fetchProducts',
+    async () => {
+        const response = await axios.get('https://fakestoreapi.com/products');
+        return response.data;
+    }
+);
+
+const productSlice = createSlice({
+    name: 'products',
+    initialState: {
+        products: [],
+        status: 'idle', // 'idle' | 'loading' | 'succeeded' | 'failed'
+        error: null,
+    },
+    reducers: {},
+    extraReducers: (builder) => {
+        builder
+            .addCase(fetchProducts.pending, (state) => {
+                state.status = 'loading';
+            })
+            .addCase(fetchProducts.fulfilled, (state, action) => {
+                state.status = 'succeeded';
+                // Add any fetched products to the array
+                state.products = action.payload;
+            })
+            .addCase(fetchProducts.rejected, (state, action) => {
+                state.status = 'failed';
+                state.error = action.error.message;
+            });
+    },
+})
+
+export default productSlice.reducer;
