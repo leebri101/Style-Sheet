3046d9462448650a3202fd6fb12009dd8b3fd304
Brian Lee <brian-lee@live.co.uk>
2025-05-26 10:45:00 +0100

    update of wishlist slicer

--- src/store/wishlistSlice.js
+++ src/store/wishlistSlice.js
@@ -1,22 +1,36 @@
 import { createSlice } from '@reduxjs/toolkit';
 
+const loadWishlistItems = () => {
+    try{
+        const savedItems = localStorage.getItem("wishlistItems");
+        return savedItems ? JSON.parse(savedItems) : [];
+    } catch (error) {
+        console.error("Error to loading wishlist from localStorage:", error);
+        return [];
+    }
+}
+
 const wishlistSlice = createSlice({
-    name: "wishlist",
+    name: 'wishlist',
     initialState: {
-        items: [],
+        items: loadWishlistItems(),
     },
-    reducers: {
-        addToWishList: (state, action) => {
-            const newItem = action.payload;
-            if (!state.items.find((item) => item.id === newItem.id)) {
-                state.items.push(newItem);
+    reducers:{
+        addToWishlist: (state, action) => {
+            const newItem = action.payload
+            if (!state.items.some((item) => item.id === newItem.id)) {
+                state.items.push(newItem)
+                // Save to localStorage
+                localStorage.setItem("wishlistItems", JSON.stringify(state.items));
             }
         },
-        removeFromWishList: (state, action) => {
-            state.items = state.items.filter((item) => item.id !== action.payload); 
+        removeFromWishlist: (state, action) => {
+            state.items = state.items.filter((item) => item.id !== action.payload)
+            // Update localStorage
+            localStorage.setItem("wishlistItems", JSON.stringify(state.items));
         },
     },
 })
 
-export const { addToWishList, removeFromWishList } = wishlistSlice.actions;
+export const { addToWishlist, removeFromWishlist } = wishlistSlice.actions;
 export default wishlistSlice.reducer;
