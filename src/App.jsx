724cf007be7dcf6a6bc5bb6107719ff01999e164
Brian Lee <brian-lee@live.co.uk>
2025-06-21 16:59:57 +0100

    Update of comps in main app page

--- src/App.jsx
+++ src/App.jsx
@@ -13,10 +13,13 @@ import RegistrationPage from "./components/user/RegistrationPage";
 import LoginPage from "./components/user/LoginPage.jsx";
 import ProfilePage from "./components/user/ProfilePage";
 import LogoutPage from "./components/user/LogoutPage";
+import Cart from "./components/cart/Cart.jsx"
 import WishlistPage from "./components/pages/WishlistPage";
 import Logo from "./components/ui/Logo"
 import ForgotPasswordPage from "./components/user/ForgotPasswordPage";
+import ResetPasswordPage from "./components/user/ResetPasswordPage.jsx"
 import SearchResultsPage from "./components/pages/SearchResultsPage.jsx";
+import CheckoutPage from "./components/pages/CheckoutPage.jsx"
 
 const App = () =>{
     return(
@@ -32,12 +35,15 @@ const App = () =>{
                             <Route path="/product/:id" element={<ProductPage />} />
                             <Route path="/register" element={<RegistrationPage />} />
                             <Route path="/login" element={<LoginPage />} />
+                            <Route path="/cart" element={<Cart />}/>
                             <Route path="/profile" element={<ProfilePage />} />
                             <Route path="/logout" element={<LogoutPage />} />
                             <Route path="/wishlist" element={<WishlistPage />} />
                             <Route path="/logo" element={<Logo />} />
                             <Route path="/forgot-password" element={<ForgotPasswordPage />} />
+                            <Route path="/reset-password" element={<ResetPasswordPage />}/>
                             <Route path="/search" element={<SearchResultsPage />} />
+                            <Route path="/checkout" element={<CheckoutPage />} />
                         </Routes>
                     </main>
                     <Footer/>
