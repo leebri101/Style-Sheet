aa60cf7d2b1934254b4db2e3cb4ef1bec7f18cd2
Brian Lee <brian-lee@live.co.uk>
2025-07-28 20:55:24 +0100

    Refactor of syntax error

--- src/components/user/ProfilePage.jsx
+++ src/components/user/ProfilePage.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
-import {logout, updateProfile} from "../../store/authSlice";
+import {logout, updateUserProfile} from "../../store/authSlice";
 import "./ProfilePage.css";
 
 //states for email and password forms
@@ -26,7 +26,7 @@ const ProfilePage = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault()
-    dispatch(updateProfile(formData))
+    dispatch(updateUserProfile(formData))
     setIsEditing(false)
   }
 
