e97310d516a3641c8463f4dd4aef1d01182e9777
Brian Lee <brian-lee@live.co.uk>
2025-08-04 20:10:15 +0100

    Update of main app

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,73 @@
-// to change 
-import { Route, Routes } from "react-router-dom";
-import { Provider } from "react-redux";
-import store from "./store/index.js";
-import Header from "./components/layout/Header";
-import Footer from "./components/layout/Footer";
-import HomePage from "./components/pages/HomePage";
-import MensPage from "./components/pages/MensPage.jsx";
-import WomensPage from "./components/pages/WomensPage";
-import KidsPage from "./components/pages/KidsPage";
-import ProductPage from "./components/pages/ProductsPage";
-import RegistrationPage from "./components/user/RegistrationPage";
-import LoginPage from "./components/user/LoginPage.jsx";
-import ProfilePage from "./components/user/ProfilePage";
-import LogoutPage from "./components/user/LogoutPage";
-import Cart from "./components/cart/Cart.jsx"
-import WishlistPage from "./components/pages/WishlistPage";
-import Logo from "./components/ui/Logo"
-import ForgotPasswordPage from "./components/user/ForgotPasswordPage";
-import ResetPasswordPage from "./components/user/ResetPasswordPage.jsx"
-import SearchResultsPage from "./components/pages/SearchResultsPage.jsx";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import { Provider } from "react-redux"
+import store from "./store"
+import Header from "./components/Header"
+import Footer from "./components/Footer"
+import HomePage from "./pages/HomePage"
+import ProductPage from "./pages/ProductPage"
+import CategoryPage from "./pages/CategoryPage"
+import MenPage from "./pages/MenPage"
+import WomenPage from "./pages/WomenPage"
+import KidsPage from "./pages/KidsPage"
+import SearchPage from "./pages/SearchPage"
+import SearchResultsPage from "./pages/SearchResultsPage"
+import WishlistPage from "./pages/WishlistPage"
+import AuthPage from "./pages/AuthPage"
+import LoginPage from "./pages/LoginPage"
+import RegistrationForm from "./components/RegistrationForm"
+import ProfilePage from "./pages/ProfilePage"
+import LogoutPage from "./pages/LogoutPage"
+import ForgotPasswordPage from "./pages/ForgotPasswordPage"
+import ResetPasswordPage from "./pages/ResetPasswordPage"
+import MockApiInitializer from "./components/MockApiInitializer"
+import "./index.css"
 
-const App = () =>{
-    return(
-        <Provider store={store}>
-                <div>
-                    <Header/>
-                    <main>
-                        <Routes>
-                            <Route path="/" element={<HomePage />} />
-                            <Route path="/mens" element={<MensPage />} />
-                            <Route path="/womens" element={<WomensPage />} />
-                            <Route path="/kids" element={<KidsPage />} />
-                            <Route path="/product/:id" element={<ProductPage />} />
-                            <Route path="/register" element={<RegistrationPage />} />
-                            <Route path="/login" element={<LoginPage />} />
-                            <Route path="/cart" element={<Cart />}/>
-                            <Route path="/profile" element={<ProfilePage />} />
-                            <Route path="/logout" element={<LogoutPage />} />
-                            <Route path="/wishlist" element={<WishlistPage />} />
-                            <Route path="/logo" element={<Logo />} />
-                            <Route path="/forgot-password" element={<ForgotPasswordPage />} />
-                            <Route path="/reset-password" element={<ResetPasswordPage />}/>
-                            <Route path="/search" element={<SearchResultsPage />} />
-                        </Routes>
-                    </main>
-                    <Footer/>
-                </div>
-        </Provider>
-    )
+function App() {
+  return (
+    <Provider store={store}>
+      <Router>
+        <div className="App">
+          <MockApiInitializer />
+          <Header />
+          <main className="main-content">
+            <Routes>
+              {/* Home Route */}
+              <Route path="/" element={<HomePage />} />
+
+              {/* Product Routes */}
+              <Route path="/product/:id" element={<ProductPage />} />
+
+              {/* Category Routes - Updated for FakeStore API categories */}
+              <Route path="/category/:category" element={<CategoryPage />} />
+              <Route path="/men" element={<MenPage />} />
+              <Route path="/women" element={<WomenPage />} />
+              <Route path="/kids" element={<KidsPage />} />
+              <Route path="/electronics" element={<CategoryPage />} />
+              <Route path="/jewelry" element={<CategoryPage />} />
+
+              {/* Search Routes */}
+              <Route path="/search" element={<SearchPage />} />
+              <Route path="/search-results" element={<SearchResultsPage />} />
+
+              {/* User Routes */}
+              <Route path="/wishlist" element={<WishlistPage />} />
+              <Route path="/auth" element={<AuthPage />} />
+              <Route path="/login" element={<LoginPage />} />
+              <Route path="/register" element={<RegistrationForm />} />
+              <Route path="/profile" element={<ProfilePage />} />
+              <Route path="/logout" element={<LogoutPage />} />
+              <Route path="/forgot-password" element={<ForgotPasswordPage />} />
+              <Route path="/reset-password" element={<ResetPasswordPage />} />
+
+              {/* Fallback Route */}
+              <Route path="*" element={<HomePage />} />
+            </Routes>
+          </main>
+          <Footer />
+        </div>
+      </Router>
+    </Provider>
+  )
 }
 
-export default App;
+export default App
